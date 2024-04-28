a4df3196483ff47f0e1857f0b3032b5b51c8772e
Ali Colver <me@alicolver.com>
2024-04-28 11:21:08 +0100

    fixing vs code integration

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
     <p>This page should either redirect you to /app or /login depending on whether you're logged in or not</p>
