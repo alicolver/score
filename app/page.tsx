88b5b774958be07d65a266ad3156b081b1d28527
Ali Colver <a.colver@btinternet.com>
2024-05-18 22:55:34 +0100

    fix: removing api call

--- app/page.tsx
+++ app/page.tsx
@@ -29,7 +29,6 @@ export default async function Home() {
 
   return (
     <>
-      {wrappedCreate()}
       <p>This page should either redirect you to /app or /login depending on whether you're logged in or not</p>
     </>
   );
