fddc5593cf43fca4c63928f69245f31c02478c84
Ali Colver <me@alicolver.com>
2024-05-20 15:44:32 +0100

    feat: adding in luke's personal stack

--- app/api/constants.ts
+++ app/api/constants.ts
@@ -1,2 +1,3 @@
 export const API_GATEWAY = "https://11wg9zigkk.execute-api.eu-west-2.amazonaws.com" // Prod 
 //export const API_GATEWAY = "https://y0p8b0gx6a.execute-api.eu-west-2.amazonaws.com" // Ali's personal stack
+//export const API_GATEWAY = "https://ynj83kmdo0.execute-api.eu-west-2.amazonaws.com" // Luke's personal stack
