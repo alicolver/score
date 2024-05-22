3e754637c323287cb7840eb0266dc20d41f02897
Ali Colver <a.colver@btinternet.com>
2024-05-23 00:07:30 +0100

    feat: figuring out tailwind and some text on the landing page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,7 +8,7 @@ import Cookies from "js-cookie";
 import {Input} from "@nextui-org/react";
 import {EyeFilledIcon, EyeSlashFilledIcon} from "@nextui-org/shared-icons";
 
-export default function Home() {
+export default function Login() {
 
     const [email, setEmail] = useState("")
     const [password, setPassword] = useState("")
@@ -38,7 +38,7 @@ export default function Home() {
     }
 
     return (
-        <section className="bg-gray-50 dark:bg-gray-900">
+        <section>
             <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                 <div
                     className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
+import Login from "@/app/login/page";
+
 export default async function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        PREDICTABALL.
-      </div>
-    </main>
+      <main className="flex flex-col items-center justify-between p-10">
+          <div className="z-10 w-full items-center justify-between text-sm lg:flex">
+              PREDICTABALL.LIVE
+          </div>
+          <div className="w-full text-center">
+          <div className="text-6xl mt-40">
+              Football Just Got <p
+              className="bg-gradient-to-r from-blue-600 to-green-300 inline-block text-transparent bg-clip-text">Funner</p>
+          </div>
+          <div>
+              A Euro 2024 Score Predictor.
+          </div>
+          </div>
+      </main>
   );
 }
