cad86c1ef4c37b972f755dba42c740aac321a4d8
Ali Colver <a.colver@btinternet.com>
2024-05-22 20:38:13 +0100

    feat: adding in nextui

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,26 +1,27 @@
-import type { Metadata } from "next";
-import { Montserrat } from "next/font/google";
+import type {Metadata} from "next";
+import {Montserrat} from "next/font/google";
 import "./globals.css";
 import Footer from "./components/footer";
+import {NextUIProvider} from "@nextui-org/react";
 
-const montserrat = Montserrat({ subsets: ["latin"] });
+const montserrat = Montserrat({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-  title: "Predictaball",
-  description: "A score predictor for Euro 2024",
+    title: "Predictaball",
+    description: "A score predictor for Euro 2024",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
+export default function RootLayout({children}: Readonly<{
+    children: React.ReactNode;
 }>) {
-  return (
-    <html lang="en">
-      <body className={montserrat.className}>
-        {children}
-        <Footer/>
-      </body>
-    </html>
-  );
+    return (
+        <html lang="en">
+        <NextUIProvider>
+            <body className={montserrat.className}>
+            {children}
+            <Footer/>
+            </body>
+        </NextUIProvider>
+        </html>
+    );
 }
