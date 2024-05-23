a3b3d473d6c7ea34b7b70e44c82ce6330d56ac74
Ali Colver <a.colver@btinternet.com>
2024-05-23 23:00:22 +0100

    refactor: removing import and fixing indentation

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,18 @@
-import Login from "@/app/login/page";
-
 export default async function Home() {
-  return (
-      <main className="flex flex-col items-center justify-between p-10">
-          <div className="z-10 w-full items-center justify-between text-sm lg:flex">
-              PREDICTABALL.LIVE
-          </div>
-          <div className="w-full text-center">
-          <div className="text-6xl mt-40">
-              Football Just Got <p
-              className="bg-gradient-to-r from-blue-600 to-green-300 inline-block text-transparent bg-clip-text">Funner</p>
-          </div>
-          <div>
-              A Euro 2024 Score Predictor.
-          </div>
-          </div>
-      </main>
-  );
+    return (
+        <main className="flex flex-col items-center justify-between p-10">
+            <div className="z-10 w-full items-center justify-between text-sm lg:flex">
+                PREDICTABALL.LIVE
+            </div>
+            <div className="w-full text-center">
+                <div className="text-6xl mt-40">
+                    Football Just Got <p
+                    className="bg-gradient-to-r from-blue-600 to-green-300 inline-block text-transparent bg-clip-text">Funner</p>
+                </div>
+                <div>
+                    A Euro 2024 Score Predictor.
+                </div>
+            </div>
+        </main>
+    );
 }
