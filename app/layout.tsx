86694cc81e9a5d08b0f39739187272f4b313d319
Ali Colver <a.colver@btinternet.com>
2024-05-23 23:28:59 +0100

    fix: adding explicit react imprt

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type {Metadata} from "next";
 import {Montserrat} from "next/font/google";
 import "./globals.css";
 import {Providers} from "@/app/providers";
+import React from "react";
 
 const montserrat = Montserrat({subsets: ["latin"]});
 
