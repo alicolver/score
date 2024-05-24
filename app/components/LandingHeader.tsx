e2a49e9ff08aa5c1478ad585df7d0389034e989e
Ali Colver <a.colver@btinternet.com>
2024-05-24 23:17:30 +0100

    🐛 Setting max container width

--- app/components/LandingHeader.tsx
+++ app/components/LandingHeader.tsx
@@ -5,7 +5,7 @@ import React from "react";
 import {navigateTo} from "@/app/actions";
 
 export function Header() {
-    return <div className="z-10 w-full items-center justify-between text-sm flex">
+    return <div className="z-10 w-full items-center justify-between text-sm flex max-w-3xl">
         PREDICTABALL.LIVE
         <Button
             className={"bg-gradient-to-tr from-blue-600 to-green-300 inline-block shadow-lg text-white"}
