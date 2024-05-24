de4347e691daf8c4901082cb7095fd9fa7e3e821
Luke Ely <elyluk@amazon.com>
2024-05-24 23:46:58 +0100

    Fix login button

--- app/components/LandingHeader.tsx
+++ app/components/LandingHeader.tsx
@@ -3,12 +3,12 @@ import React from "react";
 import Link from "next/link";
 
 export function Header() {
-    return <div className="z-10 w-full items-center justify-between text-sm flex max-w-3xl">
+    return <div className="z-50 w-full items-center justify-between text-sm flex max-w-3xl">
         PREDICTABALL.LIVE
-        <Button className={"bg-gradient-to-tr from-blue-600 to-green-300 inline-block shadow-lg text-white"}>
-            <Link href="/login">
+        <Link href="/login">
+            <Button className={"bg-gradient-to-tr from-blue-600 to-green-300 inline-block shadow-lg text-white"}>
                 Login
-            </Link>
-        </Button>
+            </Button>
+        </Link>
     </div>;
 }
