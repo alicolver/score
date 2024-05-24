9cbecd68869d8dd8ebca374a20a82ab776baf86b
Ali Colver <a.colver@btinternet.com>
2024-05-24 23:04:15 +0100

    💄 Fixing the animation with flex

--- app/page.tsx
+++ app/page.tsx
@@ -5,9 +5,10 @@ import {Header} from "@/app/components/LandingHeader";
 
 export default async function Home() {
     return (
-        <main className="dark:bg-gray-900 flex-col items-center justify-between p-10 h-screen overflow-hidden dark:text-white">
+        <main
+            className="bg-gray-900 flex flex-col items-center justify-between p-10 h-screen overflow-hidden text-white">
             <Header/>
-            <div className="w-full text-center z-40">
+            <div className="w-full absolute text-center z-40">
                 <div className="relative top-0 text-6xl mt-40">
                     Football Just Got <p
                     className="bg-gradient-to-r from-blue-600 to-green-300 inline-block text-transparent bg-clip-text">Funner</p>
