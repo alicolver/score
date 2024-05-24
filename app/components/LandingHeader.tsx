17c9b2d07e2261a8ead3749360c1c913f3890ed0
Luke Ely <elyluk@amazon.com>
2024-05-24 23:16:58 +0100

    Make landing header server component

--- app/components/LandingHeader.tsx
+++ app/components/LandingHeader.tsx
@@ -1,17 +1,14 @@
-'use client'
-
 import {Button} from "@nextui-org/react";
 import React from "react";
-import {navigateTo} from "@/app/actions";
+import Link from "next/link";
 
 export function Header() {
     return <div className="z-10 w-full items-center justify-between text-sm flex max-w-3xl">
         PREDICTABALL.LIVE
-        <Button
-            className={"bg-gradient-to-tr from-blue-600 to-green-300 inline-block shadow-lg text-white"}
-            onClick={() => navigateTo("login")}
-        >
-            Login
+        <Button className={"bg-gradient-to-tr from-blue-600 to-green-300 inline-block shadow-lg text-white"}>
+            <Link href="/login">
+                Login
+            </Link>
         </Button>
     </div>;
 }
