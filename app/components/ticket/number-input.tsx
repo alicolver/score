8336e82411717bc00cb31156018cb23fc7b3fdcd
Ali Colver <a.colver@btinternet.com>
2024-06-03 23:48:42 +0100

    ✏️ Removing input box

--- app/components/ticket/number-input.tsx
+++ app/components/ticket/number-input.tsx
@@ -27,15 +27,16 @@ export default function NumberInput(): React.JSX.Element {
         setValue(value - 1)
     }
 
-    return (<div className="flex p-2">
-        <Button disabled={value === 0} isIconOnly className="rounded-3xl mr-3 bg-gradient-to-r from-sky-500 from-10% to-emerald-500 to-90%" onClick={handleDecrease}>
+    return (<div className="flex p-2 items-center">
+        <Button disabled={value === 0} size="sm" isIconOnly className="rounded-3xl mr-3 bg-gradient-to-r from-sky-500 from-10% to-emerald-500 to-90%" onClick={handleDecrease}>
             <svg xmlns={"http://www.w3.org/2000/svg"} height="32px" viewBox="0 -960 960 960" width="32px"
                  fill="#FFFFFF">
                 <path d="M200-440v-80h560v80H200Z"/>
             </svg>
         </Button>
-        <Input isInvalid={error} variant="bordered" className="w-10 font-bold" value={value.toString()} onChange={event => handleUpdate(event)}/>
-        <Button disabled={value === 19} isIconOnly className="rounded-3xl ml-3 bg-gradient-to-r from-sky-500 from-10% to-emerald-500 to-90%" onClick={handleIncrease}>
+        {/*<Input isInvalid={error} variant="bordered" className="w-10 font-bold" value={value.toString()} onChange={event => handleUpdate(event)}/>*/}
+        <span className="font-bold text-3xl">{value}</span>
+        <Button disabled={value === 19} size="sm" isIconOnly className="rounded-3xl ml-3 bg-gradient-to-r from-sky-500 from-10% to-emerald-500 to-90%" onClick={handleIncrease}>
             <svg xmlns={"http://www.w3.org/2000/svg"} height="32px" viewBox="0 -960 960 960" width="32px"
                  fill="#FFFFFF">
                 <path d="M440-440H200v-80h240v-240h80v240h240v80H520v240h-80v-240Z"/>
