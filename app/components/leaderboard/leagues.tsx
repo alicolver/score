22807a7a60cc8fed47be3e0571e1dd144ffd1f71
Ali Colver <a.colver@btinternet.com>
2024-06-08 18:15:37 +0100

    ✨ Link to league page

--- app/components/leaderboard/leagues.tsx
+++ app/components/leaderboard/leagues.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import {getConfigWithAuthHeader} from "@/app/api/client-config";
 import {League, LeagueApi, UserApi} from "@/client";
-import { Button } from "@nextui-org/react";
+import { Button, Link } from "@nextui-org/react";
 import {BUTTON_CLASS} from "@/app/util/css-classes";
 import {getUserId} from "@/app/auth/jtw-handler";
 
@@ -43,15 +43,20 @@ export default async function Leagues(): Promise<React.JSX.Element> {
     return (
         <div className="w-full max-w-xl p-4 mb-2 text-center">
             <span className="text-white font-bold text-lg">Your Leagues</span>
-            <div className="rounded-3xl p-2 text-white" style={{
-                backgroundColor: "#21D4FD",
-                backgroundImage: "linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)"
-            }}>
-                {(await getLeagues()).map(league => (<div className="flex justify-around" key={league.leagueId}>
-                    <div className="w-2/3 font-bold">{league.name}</div>
-                    <div><span className="font-bold">Position:</span> {getPositionForLeague(league.leagueId)}</div>
-                </div>))}
-            </div>
+                {(await getLeagues()).map(league => (
+                    <div key={league.leagueId} className="w-full flex">
+                        <Link href={`app/league/${league.leagueId}/leaderboard`} className="w-full">
+                            <div className="flex w-full justify-around rounded-3xl p-2 text-white"
+                                 style={{
+                                     backgroundColor: "#21D4FD",
+                                     backgroundImage: "linear-gradient(19deg, #21D4FD 0%, #B721FF 100%)"
+                                 }}>
+                                <div className="w-2/3 font-bold">{league.name}</div>
+                                <div><span className="font-bold">Position:</span> {getPositionForLeague(league.leagueId)}</div>
+                            </div>
+                        </Link>
+                    </div>
+                ))}
             <div className="flex w-full justify-around pt-1">
                 <Button className={BUTTON_CLASS} style={{width: "100%", marginRight: "5px"}}>
                     Join a League
