c96f535780209749bb27b9dc23465939ab2644b3
Ali Colver <a.colver@btinternet.com>
2024-06-09 17:30:05 +0100

    ✨ /app/leaderboard shows global leaderboard

--- app/app/leaderboard/page.tsx
+++ app/app/leaderboard/page.tsx
@@ -1,7 +1,24 @@
-export default function Home() {
-  return (
-    <p>
-      This page will show the global leaderboard.
-    </p>
-  );
+import React from "react";
+import Leaderboard from "@/app/components/leaderboard/leaderboard";
+import Link from "next/link";
+import { Button } from "@nextui-org/react";
+import {BackIcon} from "@/app/util/back";
+
+export default function Home(): React.JSX.Element {
+    return (
+        <>
+            <div className="absolute left-4 top-4">
+                <Link href="/app">
+                    <Button
+                        isIconOnly
+                    >
+                        <BackIcon/>
+                    </Button>
+                </Link>
+            </div>
+            <div className="min-h-screen p-2 w-full bg-gray-900 flex items-center justify-center">
+                <Leaderboard leagueId={"global"} limit={false}/>
+            </div>
+        </>
+    );
 }
