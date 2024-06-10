ca9a084222b6ed037ce0c1fa06cbdbcb76e9a397
Luke Ely <elyluk@amazon.com>
2024-06-10 15:58:32 +0100

    Add league share button

--- app/app/league/[leagueId]/leaderboard/page.tsx
+++ app/app/league/[leagueId]/leaderboard/page.tsx
@@ -1,8 +1,12 @@
+
+
 import React from "react";
 import Leaderboard from "@/app/components/leaderboard/leaderboard";
 import Link from "next/link";
 import { Button } from "@nextui-org/react";
 import {BackIcon} from "@/app/util/back";
+import { Toaster } from "react-hot-toast";
+import Share from "./share";
 
 export default function Home({params}: { params: { leagueId: string } }): React.JSX.Element {
     return (
@@ -15,6 +19,9 @@ export default function Home({params}: { params: { leagueId: string } }): React.
                         <BackIcon/>
                     </Button>
                 </Link>
+            </div>
+            <div className="absolute right-4 top-4">
+                <Share leagueId={params.leagueId}/>
             </div>
             <div className="min-h-screen p-2 w-full bg-gray-900 flex items-center justify-center">
                 <Leaderboard leagueId={params.leagueId} limit={false}/>
---
+++ app/app/league/[leagueId]/leaderboard/share.tsx
@@ -0,0 +1,25 @@
+'use client'
+
+import { copyToClipboard } from "@/app/util/clipboard"
+import { BUTTON_CLASS } from "@/app/util/css-classes"
+import { Button } from "@nextui-org/react"
+import toast, { Toaster } from "react-hot-toast"
+
+export default function Share({leagueId}: { leagueId: string}): React.JSX.Element {
+    const shareInvite = () => {
+        copyToClipboard(`https://www.predictaball.live/app/league/${leagueId}/join`).then( didCopy => {
+            if (didCopy) {
+                toast.success("Copied League Invite Link To Clipboard", {duration: 4000})
+            } else {
+                toast.error("Failed To Copy League Invite Link")
+            }
+        })
+    }
+
+    return (
+        <>
+            <Toaster/>
+            <Button onPress={shareInvite} size="sm" className={BUTTON_CLASS}>Share Invite</Button>
+        </>
+    )
+}
