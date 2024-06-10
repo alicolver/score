478fa077da1760c329d40e8693bd5f21c8f73ab2
Ali Colver <a.colver@btinternet.com>
2024-06-10 21:54:41 +0100

    ✨ Fixing preview data

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,8 +7,15 @@ import React from "react";
 const montserrat = Montserrat({subsets: ["latin"]});
 
 export const metadata: Metadata = {
-    title: "Predictaball",
+    title: "Predictaball.live",
     description: "A score predictor for Euro 2024",
+    openGraph: {
+        title: "Predictaball.live",
+        description: "A score predictor for Euro 2024",
+        images: {
+            url: "https://predictaball-teamflagsbucketb6ca46de-2uk8faboxd6r.s3.eu-west-2.amazonaws.com/hidden-image.png"
+        }
+    }
 };
 
 export default function RootLayout({children}: Readonly<{
