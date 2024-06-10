b6e610f3d2f56bbcd0d955adb1bff655399b561f
Simon Archer <simon.james.archer@gmail.com>
2024-06-10 20:15:30 +0100

    add emojis to info

--- app/info/page.tsx
+++ app/info/page.tsx
@@ -9,16 +9,18 @@ export default function MoreInfo(): React.JSX.Element {
             <div className="max-w-3xl absolute text-center pt-40">
                 <p className="text-2xl">About</p>
                 <p>Predictaball is a game where you are challenged to predict the scores of <span className="font-bold">every</span> game that
-                takes place during Euro 2024. The better your predictions are, the more points you will receive.
+                takes place during Euro 2024 &#9917; 
+                <p>The better your predictions are, the more points you will receive &#128175;</p>
                 </p>
-                <p>Upcoming games will be displayed for the next 2 match days, come back each day to submit your predictions.</p>
+                <p>Upcoming games will be displayed for the next 2 match days, come back each day to submit your predictions</p>
                 <p className="pt-10 text-2xl">The Prize</p>
-                <p>Bragging rights :)</p>
-                <p>Create leagues and invite friends if you want to stake a prize or come up with some forfeits.</p>
+                <p>Bragging rights &#128512;</p>
+                <p>Create leagues and invite friends if you want to stake a prize or come up with some forfeits &#127942;</p>
 
                 <p className="pt-10 text-2xl">Scoring</p>
-                <p>5 points for a correct score</p>
-                <p>2 points for a correct result</p>
+                <p>5 points for a correct score &#129306;</p>
+                <p>2 points for a correct result &#9996;</p>
+                <p>&#10024; Look out for an updated scoring system during knockout games &#10024;</p>
             </div>
         </main>
     )
