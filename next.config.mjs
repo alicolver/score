b573c7aeefb08b7dbca152ccdc5e8c344a700bd9
Luke Ely <elyluk@amazon.com>
2024-06-11 12:46:59 +0100

    Revert "Add buffer header"
    This reverts commit 9a3de0efaca6dcb2a5c79f973d44c58f478f4b1a.

--- next.config.mjs
+++ next.config.mjs
@@ -2,19 +2,6 @@
 const nextConfig = {
     images: {
         domains: ["predictaball-teamflagsbucketb6ca46de-2uk8faboxd6r.s3.eu-west-2.amazonaws.com"]
-    },
-    async headers() {
-        return [
-            {
-                source: '/:path*{/}?',
-                headers: [
-                    {
-                        key: 'X-Accel-Buffering',
-                        value: 'no',
-                    },
-                ],
-            },
-        ]
     }
 };
 
