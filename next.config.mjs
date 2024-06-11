9a3de0efaca6dcb2a5c79f973d44c58f478f4b1a
Luke Ely <elyluk@amazon.com>
2024-06-11 12:37:44 +0100

    Add buffer header

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,19 @@
 const nextConfig = {
     images: {
         domains: ["predictaball-teamflagsbucketb6ca46de-2uk8faboxd6r.s3.eu-west-2.amazonaws.com"]
+    },
+    async headers() {
+        return [
+            {
+                source: '/:path*{/}?',
+                headers: [
+                    {
+                        key: 'X-Accel-Buffering',
+                        value: 'no',
+                    },
+                ],
+            },
+        ]
     }
 };
 
