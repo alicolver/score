9d6a4e8289ff554d727d58711a9d973d8ab53d34
Ali Colver <a.colver@btinternet.com>
2024-06-12 22:44:32 +0100

    💄 Updating pagination

--- app/components/leaderboard/leaderboard-pagination.tsx
+++ app/components/leaderboard/leaderboard-pagination.tsx
@@ -1,18 +1,18 @@
 'use client'
 
-import { LeaderboardInner } from "@/client"
+import {LeaderboardInner} from "@/client"
 import LeaderboardEntry from "./leaderboard-entry"
-import { useState } from "react"
-import { Pagination } from "@nextui-org/react";
-import { BUTTON_CLASS } from "@/app/util/css-classes";
+import React, {useState} from "react"
+import {Pagination} from "@nextui-org/react";
+import {BUTTON_CLASS} from "@/app/util/css-classes";
 
-
-interface LeaderboardPaginsationProps {
+interface LeaderboardPaginationProps {
     leaderboardInners: LeaderboardInner[]
     userId: string | undefined
 }
 
-export default function LeaderboardPagination(props: LeaderboardPaginsationProps): React.JSX.Element {
+export default function LeaderboardPagination(props: LeaderboardPaginationProps): React.JSX.Element {
+
     const [currentPage, setCurrentPage] = useState(0);
     const itemsPerPage = 10;
 
@@ -28,17 +28,23 @@ export default function LeaderboardPagination(props: LeaderboardPaginsationProps
     const totalPages = Math.ceil(props.leaderboardInners.length / itemsPerPage);
 
     return <>
-        {getPaginatedLeaderboard(props.leaderboardInners).map(x => (
+        {getPaginatedLeaderboard(props.leaderboardInners).map((entry, index) => (
             <LeaderboardEntry
-                key={x.position}
-                entry={x}
-                isUser={x.user.userId === props.userId}
+                key={index}
+                entry={entry}
+                isUser={entry.user.userId === props.userId}
             />
         ))}
-        {totalPages > 1 && <Pagination showControls radius="full" total={totalPages} initialPage={1} onChange={handlePageChange} classNames={{
-            cursor:
-                BUTTON_CLASS,
-        }} />}
-
+        {totalPages > 1 &&
+            <Pagination showControls radius="full" total={totalPages} initialPage={1} onChange={handlePageChange}
+                        classNames={{
+                            cursor: BUTTON_CLASS,
+                            item: "bg-transparent text-white hover:text-black",
+                            //prev: "bg-transparent white",
+                            //next: "bg-transparent white",
+                            //forwardIcon: "white",
+                            //chevronNext: "white"
+                        }}
+            />}
     </>
 }
