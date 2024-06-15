e8363bbe6233dd01055d8ef5b85b841bb2f0fc7a
Ali Colver <a.colver@btinternet.com>
2024-06-15 23:26:01 +0100

    ✨ Adding in link to user page

--- app/app/match/[matchId]/predictions/prediction.tsx
+++ app/app/match/[matchId]/predictions/prediction.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {PredictionWithUser} from "@/client";
 import {failStyle, neutralStyle, successStyle} from "@/app/util/css-styles";
+import Link from "next/link";
 
 enum Result {
     SCORE, RESULT, NEITHER
@@ -26,22 +27,23 @@ export default function PredictionData(props: {
         return Result.NEITHER
     }
 
-    return(
-        <div
+    return (
+        <Link
             className="w-full flex items-center mt-2 max-w-xl text-center p-3 rounded-3xl animate-appearance-in animation-delay-0"
             style={MOVEMENT_TO_COLOR.get(calculateResult(props.predictionWithUser.prediction.points!))!}
+            href={`/app/user/${props.predictionWithUser.user.userId}/history?first=${props.predictionWithUser.user.firstName.replace(/\s/g,'')}&last=${props.predictionWithUser.user.familyName.replace(/\s/g,'')}`}
         >
             <div className="w-3/4 text-gray-200">
                 <div className="w-full font-bold">
                     {`${props.predictionWithUser.user.firstName} ${props.predictionWithUser.user.familyName}`}
                 </div>
-            <div>
-                {props.predictionWithUser.prediction.homeScore} - {props.predictionWithUser.prediction.awayScore}
-            </div>
+                <div>
+                    {props.predictionWithUser.prediction.homeScore} - {props.predictionWithUser.prediction.awayScore}
+                </div>
             </div>
             <div className="w-1/4 text-4xl font-bold text-gray-200">
-                    {props.predictionWithUser.prediction.points}
+                {props.predictionWithUser.prediction.points}
             </div>
-        </div>
+        </Link>
     )
 }
